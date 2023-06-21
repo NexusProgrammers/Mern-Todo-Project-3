4b3bec9c54d9c3e4cd3efea698f264881349b7c9
Jawad Ullah <jawadullah156@gmail.com>
2023-06-21 02:16:41 -0700

    first commit

--- client/src/services/todoService.js
+++ client/src/services/todoService.js
@@ -8,7 +8,7 @@ export const createTodo = createAsyncThunk(
   async ({ formData, navigate }, { rejectWithValue }) => {
     try {
       const response = await axios.post(
-        `https://mern-todo-one-plum.vercel.app/api/v1/todos/create`,
+        `http://localhost:4000/api/v1/todos/create`,
         formData
       );
       toast.success(response.data.message, {
@@ -37,7 +37,7 @@ export const deleteTodo = createAsyncThunk(
   async ({ _id }, { rejectWithValue }) => {
     try {
       const response = await axios.delete(
-        `https://mern-todo-one-plum.vercel.app/api/v1/todos/delete/${_id}`
+        `http://localhost:4000/api/v1/todos/delete/${_id}`
       );
       toast.success(response.data.message, {
         duration: 2000,
@@ -64,7 +64,7 @@ export const deleteAllTodo = createAsyncThunk(
   async (_, { rejectWithValue }) => {
     try {
       const response = await axios.delete(
-        `https://mern-todo-one-plum.vercel.app/api/v1/todos/all/delete`
+        `http://localhost:4000/api/v1/todos/all/delete`
       );
       toast.success(response.data.message, {
         duration: 2000,
@@ -91,7 +91,7 @@ export const updateTodo = createAsyncThunk(
   async ({ _id, formData }, { rejectWithValue }) => {
     try {
       const response = await axios.patch(
-        `https://mern-todo-one-plum.vercel.app/api/v1/todos/update/${_id}`,
+        `http://localhost:4000/api/v1/todos/update/${_id}`,
         formData
       );
       toast.success(response.data.message, {
@@ -119,7 +119,7 @@ export const getTodo = createAsyncThunk(
   async (_, { rejectWithValue }) => {
     try {
       const response = await axios.get(
-        `https://mern-todo-one-plum.vercel.app/api/v1/todos/one/`
+        `http://localhost:4000/api/v1/todos/one/`
       );
       return response.data.todos;
     } catch (error) {
@@ -143,7 +143,7 @@ export const getTodos = createAsyncThunk(
   async (_, { rejectWithValue }) => {
     try {
       const response = await axios.get(
-        `https://mern-todo-one-plum.vercel.app/api/v1/todos/all`
+        `http://localhost:4000/api/v1/todos/all`
       );
       return response.data.todos;
     } catch (error) {
